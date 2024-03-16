ea9d54a6f4f0665c3aa26432842fcae871378435
PixelVerseIT <hayden.kong@pembroke.sa.edu.au>
2024-03-17 10:07:41 +1030

    Updated @PixelVerseIT

--- L7/4-5.cpp
+++ L7/4-5.cpp
@@ -4,21 +4,14 @@ int main(){
     int year;
     printf("Enter a year: ");
     scanf("%d", &year);
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        printf("The year is a leap year\n");
-    } else {
-        printf("The year is not a leap year\n");
-        int yesno;
-        printf("Do you want to check if the year is a leap year again? (1 for yes, 0 for no): ");
-        scanf("%d", &yesno);
-        if (yesno == 1) {
-            printf("Enter a year: ");
-            scanf("%d", &year);
-            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                printf("The year is a leap year\n");
-            } else {
-                printf("The year is not a leap year\n");
-            }
+    while (year != 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            printf("The year is a leap year\n");
+        } else {
+            printf("The year is NOT a leap year\n");
         }
+        printf("Enter a year: ");
+        scanf("%d", &year);
     }
+    return 0;
 }
